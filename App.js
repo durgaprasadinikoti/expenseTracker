2259d519f183182558d0c1b483a669d87a07244a
Ranga Komarthi <88224031+komarthiranga@users.noreply.github.com>
2023-05-03 11:28:11 +0530

    Added Index screen to the app

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import ExpenseProvider from "./store/ExpenseProvider";
 import Navigation from "./Navigation/Navigation";
+import { useState } from 'react';
+import { Button, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [ mainScreen, setMainScreen ] = useState(false);
 
   return (
     <ExpenseProvider>
       <StatusBar style="light" />
-      <Navigation />
+      { !mainScreen && <SafeAreaView><Button title="Navigate to Main Screen" onPress={ () => setMainScreen(true) } /></SafeAreaView> }
+      {mainScreen && <Navigation /> }
     </ExpenseProvider>
   );
 }
---
+++ screens/Index.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text, Button } from 'react-native';
+const IndexScreen = () => {
+    return(
+        <SafeAreaView>
+            <Text>This is Index Screen</Text>
+            <Button />
+        </SafeAreaView>
+    )
+}
+
+export default IndexScreen;
